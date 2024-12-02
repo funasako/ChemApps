890d0237d53eed360e282ef8a01adafdbcfa2b20
funasako <157328833+funasako@users.noreply.github.com>
2024-12-02 17:47:46 +0900

    Update ChemApps.py

--- ChemApps.py
+++ ChemApps.py
@@ -1,13 +1,12 @@
 import streamlit as st
 
-# タイトル等
-st.set_page_config(page_title="ChemApps", page_icon=":bar_chart:", )
-st.title("ChemApps")
-st.write("左のメニューからアプリを選択")
-
 with st.sidebar:
     st.page_link("pages/01_uv-vis.py", label="UV-vis to Excel", icon="📊")
     st.page_link("pages/02_ir.py", label="IR to Excel", icon="📊")
     st.page_link("pages/03_gausslog.py", label="GaussLog", icon="📄")
 
 
+# タイトル等
+st.set_page_config(page_title="ChemApps", page_icon=":bar_chart:", )
+st.title("ChemApps")
+st.write("左のメニューからアプリを選択")
