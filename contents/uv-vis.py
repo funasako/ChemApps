182eda01cf73dff6829738ae365bc2ddb76f8436
funasako <157328833+funasako@users.noreply.github.com>
2024-12-02 19:16:17 +0900

    Update uv-vis.py

--- contents/uv-vis.py
+++ contents/uv-vis.py
@@ -9,7 +9,7 @@ import os
 
 
 # タイトル等
-# st.set_page_config(page_title="UV-vis | JASCO Spectra Formatter", page_icon=":bar_chart:", )
+st.set_page_config(page_title="UV-vis | JASCO Spectra Formatter", page_icon=":bar_chart:", )
 st.title("UV-vis | JASCO Spectra File to Excel")
 st.markdown("**:blue[※動作にはインターネット接続が必要です。]**")
 st.write("1. 装置が書き出したテキスト形式ファイルを用意する、もしくは、スペクトルマネージャーでテキストファイルをエクスポートする（ファイル名をしっかりつけておく）")
