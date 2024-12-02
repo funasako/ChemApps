bf257c023d2c5d3aa5077b7864fe036de86f5627
funasako <157328833+funasako@users.noreply.github.com>
2024-12-02 18:41:12 +0900

    Update ChemApps.py

--- ChemApps.py
+++ ChemApps.py
@@ -3,10 +3,20 @@ import streamlit as st
 # タイトル
 st.set_page_config(page_title="ChemApps", page_icon=":test_tube:", )
 
+# サイドバー
+st.sidebar.title("ページナビゲーション")
+uv_vis = st.sidebar.button("📊 UV-vis | JASCO .txt to xlsx")
+ir = st.sidebar.button("📊 Infrared | JASCO .txt to xlsx")
+gausslog = st.sidebar.button("📄 DFT | GaussLog")
 
-# sidebar
-uv_vis = st.Page(page="contents/uv-vis.py", title="UV-vis | JASCO .txt to xlsx", icon="📊")
-ir = st.Page(page="contents/ir.py", title="Infrared | JASCO .txt to xlsx", icon="📊")
-gausslog = st.Page(page="contents/gausslog.py", title="DFT | GaussLog", icon="📄")
-pg = st.navigation([uv_vis, ir, gausslog])
-pg.run()
+# コメントの追加
+st.sidebar.markdown("---")  # 区切り線を挿入
+st.sidebar.write("各ツールはJASCOデータの変換と分析をサポートします。")
+
+# ボタンの動作設定
+if uv_vis:
+    st.experimental_set_query_params(page="uv-vis")
+elif ir:
+    st.experimental_set_query_params(page="ir")
+elif gausslog:
+    st.experimental_set_query_params(page="gausslog")
