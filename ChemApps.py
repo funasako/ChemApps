470bca1c4e796d92deeb7eadcedb6ef384d892c7
funasako <157328833+funasako@users.noreply.github.com>
2024-12-02 18:23:16 +0900

    Update ChemApps.py

--- ChemApps.py
+++ ChemApps.py
@@ -5,8 +5,8 @@ st.set_page_config(page_title="ChemApps", page_icon=":bar_chart:", )
 
 
 # sidebar
-uv_vis = st.Page(page="contents/uv-vis.py", title="UV-vis to Excel", icon="📊")
-ir = st.Page(page="contents/ir.py", title="IR to Excel", icon="📊")
-gausslog = st.Page(page="contents/gausslog.py", title="GaussLog", icon="📄")
+uv_vis = st.Page(page="contents/uv-vis.py", title="UV-vis | JASCO .txt > xlsx", icon="📊")
+ir = st.Page(page="contents/ir.py", title="IR  | JASCO .txt > xlsx", icon="📊")
+gausslog = st.Page(page="contents/gausslog.py", title="DFT | GaussLog", icon="📄")
 pg = st.navigation([uv_vis, ir, gausslog])
 pg.run()
