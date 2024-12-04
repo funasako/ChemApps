9af0de14c2fad75249b7c111f31e331b44923a54
funasako <157328833+funasako@users.noreply.github.com>
2024-12-04 20:45:16 +0900

    Update uv-vis.py

--- contents/uv-vis.py
+++ contents/uv-vis.py
@@ -11,13 +11,14 @@ import pytz
 
 # タイトル等
 if 'multi_page' not in st.session_state:
-    st.set_page_config(page_title="UV-vis | JASCO Spectra File to Excel", page_icon=":bar_chart:", )
-st.title("UV-vis | JASCO Spectra File to Excel")
+    st.set_page_config(page_title="UV-vis | Spectra File to Excel", page_icon=":bar_chart:", )
+st.title("UV-vis | Spectra File to Excel")
 st.markdown("**:blue[※動作にはインターネット接続が必要です。]**")
 st.write("1. 装置が書き出したテキスト形式ファイルを用意する、もしくは、スペクトルマネージャーでテキストファイルをエクスポートする（ファイル名をしっかりつけておく）")
 st.write("2. 以下にドラッグ&ドロップしてグラフ表示。複数ファイルからプロット重ね書きも可能")
 st.write("3. Excelファイルをダウンロード")
 st.write("4. 別のExcelファイルを作成する場合は、ページを再読込するかアップロード済みファイルをすべて✕ボタンで削除する")
+st.markdown(":red[※対応フォーマット：日本分光（JASCO）、日立ハイテク]")
 st.write("")
 
 # 表示用グラフの作成
