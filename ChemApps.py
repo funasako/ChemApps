9b1731f8d46b3eb4cdb6d5dbd474b0cd1c251670
funasako <157328833+funasako@users.noreply.github.com>
2024-12-04 20:48:20 +0900

    Update ChemApps.py

--- ChemApps.py
+++ ChemApps.py
@@ -7,8 +7,8 @@ st.set_page_config(page_title="ChemApps", page_icon=":test_tube:", )
 st.session_state['multi_page'] = True  # フラグをTrueに設定
 
 # sidebar
-uv_vis = st.Page(page="contents/uv-vis.py", title="UV-vis | JASCO .txt to xlsx", icon="📊")
-ir = st.Page(page="contents/ir.py", title="Infrared | JASCO .txt to xlsx", icon="📊")
+uv_vis = st.Page(page="contents/uv-vis.py", title="UV-vis | JASCO/HITACHI txt to xlsx", icon="📊")
+ir = st.Page(page="contents/ir.py", title="Infrared | JASCO txt to xlsx", icon="📊")
 gausslog = st.Page(page="contents/gausslog.py", title="DFT | GaussLog", icon="📄")
 pg = st.navigation([uv_vis, ir, gausslog])
 pg.run()
